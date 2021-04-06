fixed loss scaling and softmax

test_mnist.py
@@ -6,7 +6,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import transforms
 from torchvision.datasets import FashionMNIST
+from torch.optim.lr_scheduler import StepLR
+import pandas as pd
+import seaborn as sns
 from torch.utils.data import DataLoader
+
 
 os.environ["CUDA_DEVICE_ORDER"] = 'PCI_BUS_ID'
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
@@ -26,15 +30,22 @@ class Model(torch.nn.Module):
         self.softmax = vdp.Softmax()
 
     def forward(self, x):
+        # x = x.view(1000, -1)
         mu, sigma = self.layer_1(x)
-        mu = self.bn1(mu)
+        # print(mu)
+        # mu = self.bn1(mu)
         mu, sigma = self.relu(mu, sigma)
+        # print(mu)
         mu, sigma = self.layer_2(mu, sigma)
-        mu = self.bn2(mu)
+        # print(mu)
+        # mu = self.bn2(mu)
         mu, sigma = self.relu(mu, sigma)
+        # print(mu)
         mu, sigma = self.layer_3(mu, sigma)
-        mu = self.bn3(mu)
+        # print(mu)
+        # mu = self.bn3(mu)
         mu, sigma = self.softmax(mu, sigma)
+        # print(mu)
         return mu, sigma
 
     def score(self, logits, y):
@@ -51,39 +62,67 @@ def add_noise(s, snr):
 
 
 def main():
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    mnist_train = FashionMNIST(os.getcwd(), train=True, download=True, transform=transform)
-    mnist_test = FashionMNIST(os.getcwd(), train=False, download=True, transform=transform)
-    x_train, y_train = mnist_train.data.view(60000, -1), mnist_train.targets
-    x_test, y_test = mnist_test.data.view(10000, -1), mnist_test.targets
+    # transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    mnist_train = FashionMNIST(os.getcwd(), train=True, download=True, transform=None)
+    mnist_test = FashionMNIST(os.getcwd(), train=False, download=True, transform=None)
+    # trainloader = DataLoader(mnist_train, batch_size=1000, num_workers=2,
+    #                          shuffle=True)  # IF YOU CAN FIT THE DATA INTO MEMORY DO NOT USE DATALOADERS
+    # testloader = DataLoader(mnist_test, batch_size=1000, num_workers=2, shuffle=True)
+    x_train, y_train = mnist_train.data.view(60000, -1)/255.0, mnist_train.targets
+    x_test, y_test = mnist_test.data.view(10000, -1)/255.0, mnist_test.targets
     model = Model()
-    no_epochs = 100
-    optimizer = torch.optim.Adam(model.parameters(), lr=8e-2)
+    no_epochs = 200
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, amsgrad=True)
     model.to('cuda:0')
     train_accs = []
     test_accs = []
     start_time = time.time()
+    log_det, likelihood, kl = list(), list(), list()
     for epoch in range(no_epochs):
+        # for itr, (x_train, y_train) in enumerate(trainloader):
         model.train()
         total_loss = 0
         optimizer.zero_grad()
         mu, sigma = model.forward(x_train.float().to('cuda:0'))
-        loss = vdp.ELBOLoss(mu, sigma, y_train.to('cuda:0'))-2*(model.layer_1.kl_term()+model.layer_2.kl_term()+model.layer_3.kl_term())
+        log_det_i, likelihood_i = vdp.ELBOLoss(mu, sigma, y_train.to('cuda:0'))
+        kl_i = (model.layer_1.kl_term()+model.layer_2.kl_term()+model.layer_3.kl_term())
+        loss = 1*log_det_i+likelihood_i-0.001*(kl_i)
         total_loss += loss.item()
         loss.backward()
-        init_norms_mu = [torch.norm(model.layer_1.mu.weight), torch.norm(model.layer_2.mu.weight), torch.norm(model.layer_3.mu.weight)]
-        init_norms = [torch.norm(model.layer_1.sigma.weight), torch.norm(model.layer_2.sigma.weight), torch.norm(model.layer_3.sigma.weight)]
-        # print(model.layer_1.mu.weight.grad)
-        # print(model.layer_1.sigma.weight.grad)
         optimizer.step()
+        log_det.append(log_det_i.detach().cpu().numpy())
+        likelihood.append(likelihood_i.detach().cpu().numpy())
+        kl.append(kl_i.detach().cpu().numpy())
         train_acc = model.score(mu, y_train.to('cuda:0'))
         print('Epoch {}/{}: Training Loss: {:.2f}'.format(epoch + 1, no_epochs, total_loss))
         print('Train Accuracy: {:.2f}'.format(train_acc))
         train_accs.append(train_acc)
+        # if (epoch % 25) == 0 or (epoch == 49):
+        #     sns.kdeplot(pd.DataFrame(model.layer_1.mu.weight.view(-1, 1).detach().cpu().numpy(), columns=['mu'])[
+        #                     'mu'], shade=True)
+        #     plt.savefig('plots/mu_weights_epoch_{}.png'.format(epoch))
+        #     plt.clf()
+        #     sns.kdeplot(pd.DataFrame(model.layer_1.sigma.weight.view(-1, 1).detach().cpu().numpy(), columns=['sigma'])[
+        #                     'sigma'], shade=True)
+        #     plt.savefig('plots/sigma_weights_epoch_{}.png'.format(epoch))
+        #     plt.clf()
+        #     sns.kdeplot(pd.DataFrame(model.layer_1.mu.weight.grad.view(-1, 1).detach().cpu().numpy(), columns=['mu gradient'])[
+        #                     'mu gradient'], shade=True)
+        #     plt.savefig('plots/mu_grad_epoch_{}.png'.format(epoch))
+        #     plt.clf()
+        #     sns.kdeplot(pd.DataFrame(model.layer_1.sigma.weight.grad.view(-1, 1).detach().cpu().numpy(), columns=['sigma gradient'])[
+        #                     'sigma gradient'], shade=True)
+        #     plt.savefig('plots/sigma_grad_epoch_{}.png'.format(epoch))
+        #     plt.clf()
+        # print(model.layer_1.mu.weight.grad)
+        # print(model.layer_1.sigma.weight.grad)
+        # for itr, (x_test, y_test) in enumerate(trainloader):
         model.eval()  # This removes stuff like dropout and batch norm for inference stuff
         total_loss = 0
         mu, sigma = model.forward(x_test.float().to('cuda:0'))
-        loss = vdp.ELBOLoss(mu, sigma, y_test.to('cuda:0'))-2*(model.layer_1.kl_term()+model.layer_2.kl_term())
+        log_det_i, likelihood_i = vdp.ELBOLoss(mu, sigma, y_test.to('cuda:0'))
+        kl_i = (model.layer_1.kl_term()+model.layer_2.kl_term()+model.layer_3.kl_term())
+        loss = log_det_i+likelihood_i+kl_i
         total_loss += loss.item()
         test_acc = model.score(mu, y_test.to('cuda:0'))
         print('Test Loss: {:.2f},    Test Accuracy: {:.2f}'.format(total_loss, test_acc))
@@ -91,7 +130,22 @@ def main():
     plt.plot(np.arange(no_epochs), train_accs, label='Train Accuracy')
     plt.plot(np.arange(no_epochs), test_accs, label='Test Accuracy')
     plt.xlabel('Epoch')
-    plt.ylabel('Loss')
+    plt.ylabel('Accuracy')
+    plt.title('Fashion MNIST: Small Network VDP++')
+    plt.legend()
+    plt.show()
+    plt.plot(np.arange(no_epochs), log_det, label='Log determinant')
+    plt.xlabel('Epoch')
+    plt.title('Fashion MNIST: Small Network VDP++')
+    plt.legend()
+    plt.show()
+    plt.plot(np.arange(no_epochs), likelihood, label='Likelihood')
+    plt.xlabel('Epoch')
+    plt.title('Fashion MNIST: Small Network VDP++')
+    plt.legend()
+    plt.show()
+    plt.plot(np.arange(no_epochs), kl, label='KL terms')
+    plt.xlabel('Epoch')
     plt.title('Fashion MNIST: Small Network VDP++')
     plt.legend()
     plt.show()
@@ -101,17 +155,17 @@ def main():
     for snr in range(len(snrs)):
         x_noisy = add_noise(x_test.cpu().numpy(), snrs[snr])
         mu, sigma = model.forward(torch.from_numpy(x_noisy).float().to('cuda:0'))
-        sigmas.append(torch.mean(torch.mean(torch.abs(sigma), dim=1)).detach().cpu().numpy())
+        sigmas.append(torch.mean(torch.mean(sigma, dim=1)).detach().cpu().numpy())
     plt.figure()
     plt.plot(snrs, sigmas)
     plt.xlabel('SNR (dB)')
-    plt.ylabel('Mean Mean Absolute Test Sigma')
+    plt.ylabel('Mean Mean Test Sigma')
     plt.title('Fashion MNIST: Small network VDP++')
     plt.show()
-    print(init_norms_mu)
-    print([torch.norm(model.layer_1.mu.weight), torch.norm(model.layer_2.mu.weight), torch.norm(model.layer_3.mu.weight)])
-    print(init_norms)
-    print([torch.norm(model.layer_1.sigma.weight), torch.norm(model.layer_2.sigma.weight), torch.norm(model.layer_3.sigma.weight)])
+    # print(init_norms_mu)
+    # print([torch.norm(model.layer_1.mu.weight), torch.norm(model.layer_2.mu.weight), torch.norm(model.layer_3.mu.weight)])
+    # print(init_norms)
+    # print([torch.norm(model.layer_1.sigma.weight), torch.norm(model.layer_2.sigma.weight), torch.norm(model.layer_3.sigma.weight)])
     pass
 
 

vdp.py
@@ -11,8 +11,8 @@ class Linear(torch.nn.Module):
         self.input_flag = input_flag
         self.mu = torch.nn.Linear(in_features, out_features, bias)
         self.sigma = torch.nn.Linear(in_features, out_features, bias)
-        torch.nn.init.normal_(self.mu.weight, mean=0, std=0.1)
-        torch.nn.init.uniform_(self.sigma.weight, a=0, b=4)
+        torch.nn.init.xavier_normal_(self.mu.weight)
+        torch.nn.init.uniform_(self.sigma.weight, a=0, b=10)
 
 
     def forward(self, mu_x, sigma_x=torch.tensor(0., requires_grad=True)):
@@ -22,9 +22,9 @@ class Linear(torch.nn.Module):
             pass
         else:
             mu_y = self.mu(mu_x)
-            sigma_y = (sigma_x @ logexp(self.sigma.weight).T) +\
-                      (mu_x**2 @ logexp(self.sigma.weight).T) + \
-                      (self.mu.weight**2 @ sigma_x.T).T + self.sigma.bias
+            sigma_y = (logexp(self.sigma.weight) @ sigma_x.T).T + \
+                      (self.mu.weight**2 @ sigma_x.T).T + \
+                      (mu_x**2 @ logexp(self.sigma.weight).T) + self.sigma.bias
             pass
         return mu_y, sigma_y
 
@@ -54,16 +54,22 @@ class Softmax(torch.nn.Module):
         # This is sorta incorrect. It will sum the rows of the product instead of
         # giving us just the diagonal elements of the product...
         # need to find a way to compute J**2 @ sigma without directly computing J
-        jvp = torch.autograd.functional.jvp(self.softmax, mu, sigma, create_graph=True)[1]
-        sigma = torch.autograd.functional.vjp(self.softmax, mu, jvp, create_graph=True)[1]
+        # jvp = torch.autograd.functional.jvp(self.softmax, mu, sigma, create_graph=True)[1]
+        # sigma = torch.autograd.functional.vjp(self.softmax, mu, jvp, create_graph=True)[1]
+        # print(mu)
         mu = self.softmax(mu)
+        # print(mu)
+        J = mu*(1-mu)
+        sigma = (J**2) * sigma
         return mu, sigma
 
 
 def ELBOLoss(mu, sigma, y):
-    y_hot = torch.nn.functional.one_hot(y)
-    sigma_clamped = torch.log(1+torch.exp(torch.clamp(sigma, 0, 87)))
-    log_det = torch.log(torch.prod(sigma_clamped, dim=1))
-    likelihood = 0.5*torch.sum(((y_hot-mu)**2).T @ torch.reciprocal(sigma_clamped))
-    loss = (log_det+likelihood)
-    return torch.mean(loss)
+    y_hot = torch.nn.functional.one_hot(y, num_classes=10)
+    num_samples = y_hot.shape[0]
+    sigma_clamped = torch.log(1+torch.exp(torch.clamp(sigma, 0, 10)))
+    # print(torch.sum(sigma))
+    # print(torch.sum(sigma_clamped))
+    log_det = (1/num_samples)*torch.log(torch.prod(sigma_clamped, dim=1))
+    likelihood = (1/num_samples)*(0.5*torch.sum(((y_hot-mu)**2).T @ torch.reciprocal(sigma_clamped)))
+    return torch.mean(log_det), likelihood